30080a141587769647de063273812eb4794c73e6
Anilkalbi <141136121+Anilkalbi@users.noreply.github.com>
2024-01-23 20:06:58 +0530

    Add files via upload

---
+++ arithmetic.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a,b,sum,diff,mul,div;
+    cout<<"enter the number:";
+    cin>>a>>b;
+    sum=a+b; 
+    cout<<sum<<endl;
+    diff=a-b;
+      cout<<diff<<endl;
+    mul=a*b;
+      cout<<mul<<endl;
+    div=a/b;
+      cout<<div<<endl;
+}
