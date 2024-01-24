434d5da219507b40eaaa204f309fa64574d64f2e
Anilkalbi <141136121+Anilkalbi@users.noreply.github.com>
2024-01-24 21:33:43 +0530

    Add files via upload

---
+++ relation.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int s1,s2;
+    cout<<"enter the string:";
+    cin>>s1>>s2;
+    if(s1>s2){
+        cout<<"s1 is greater than  to s2:";
+        }
+    if(s1<s2){
+        cout<<"s1 is lesser than to s2:";
+    }
+    if(s1<=s2){
+        cout<<""
+
+    }
+    if(s1>=s2){
+
+    }
+    return 0;
+    
+}
