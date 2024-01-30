390b2348300c46d73ad41fb7118f8cbc2df41efc
Anilkalbi <141136121+Anilkalbi@users.noreply.github.com>
2024-01-30 21:07:18 +0530

    Add files via upload

---
+++ ifinitializer.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int value=7;
+    if(value==7)
+    {
+        cout<<"value is"<<value<<endl;
+    }
+    else
+    {
+        cout<<"value is not "<<value<<endl;
+    }
+    int value1=13;
+     if(value1==9 )
+    {
+        cout<<"value is"<<value1<<endl;
+    }
+    else
+    {
+        cout<<"value is not "<<value1<<endl;
+    }
+}
